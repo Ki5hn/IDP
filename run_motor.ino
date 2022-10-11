48798c440c60932c1900022445824de3774ef6e8
ki5hn <kmz24@cam.ac.uk>
2022-10-11 10:03:08 +0100

    updates

--- run_motor.ino
+++ run_motor.ino
@@ -2,16 +2,48 @@
 #include <Adafruit_MotorShield.h>
 #include "utility/Adafruit_MS_PWMServoDriver.h"
 
-void setup() {
-  // put your setup code here, to run once:
 Adafruit_MotorShield AFMS = Adafruit_MotorShield(); 
 Adafruit_DCMotor *myMotor = AFMS.getMotor(1);
-AFMS.begin();
-myMotor->setSpeed(150);
-myMotor->run(FORWARD);
+char sensorPin = A0;
+int sensorValue = 0;
+int line_detect_left = analogRead(A4);
+int line_detect_right = analogRead(A3);
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+  AFMS.begin();
+  myMotor->setSpeed(0);
+  myMotor->run(FORWARD);
+
+
 }
 
 void loop() {
+
+
+  sensorValue = analogRead(sensorPin);
+  Serial.println(sensorValue);
+
+  
+  if (line_detect_left > 600){
+    motor_left->setSpeed(300);
+  }
+  else{
+    motor_left->setSpeed(200); 
+
+  }
+
+    if (line_detect_right > 600){
+    motor_left->setSpeed(300);
+  }
+  else{
+    motor_left->setSpeed(200); 
+
+  }
+  
   // put your main code here, to run repeatedly:
+  
+
 
 }
